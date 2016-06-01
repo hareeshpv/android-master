f1c5a411fe84eaf87dde6a4e5a31986725ec590d
raaga <pvharish@pvharish-PC>
2016-06-01 21:20:52 +0530

    Test commit

--- app/src/test/java/com/apppractise/app1/ExampleUnitTest.java
+++ app/src/test/java/com/apppractise/app1/ExampleUnitTest.java
@@ -7,6 +7,8 @@ import static org.junit.Assert.*;
 /**
  * To work on unit tests, switch the Test Artifact in the Build Variants view.
  */
+
+
 public class ExampleUnitTest {
     @Test
     public void addition_isCorrect() throws Exception {
